b6297a04b4e7b312164122c49d43fd0983397096
codep1ltio <joonasmikael5@gmail.com>
2026-05-09 00:32:13 +0700

    Update injectshaders.js

--- scripts/features/injectshaders.js
+++ scripts/features/injectshaders.js
@@ -1,6 +1,7 @@
 // Made by Slime King
 (() => {
-  const scriptURL = chrome.runtime.getURL('scripts/features/shaders.js');
+  const ext = chrome || browser;
+  const scriptURL = ext.runtime.getURL('scripts/features/shaders.js');
   if (document.querySelector(`script[src="${scriptURL}"]`)) return;
 
   const script = document.createElement('script');
